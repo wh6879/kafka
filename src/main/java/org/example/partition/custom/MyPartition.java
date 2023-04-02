ad5a862771ce1080f36958408da20d6875cdf7e5
wh6879 <wh6879@aliyun.com>
2023-04-02 18:29:22 +0800

    auth: wh commit: ProducerPartCustom

---
+++ src/main/java/org/example/partition/custom/MyPartition.java
@@ -0,0 +1,31 @@
+package org.example.partition.custom;
+
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.common.Cluster;
+
+import java.util.Map;
+
+public class MyPartition implements Partitioner {
+    @Override
+    public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
+        String msg = value.toString();
+        int partition = 0;
+        if (msg.contains("abc")) {
+            partition = 1;
+        }
+        if (msg.contains("123")) {
+            partition = 2;
+        }
+        return partition;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs) {
+
+    }
+}
