027a6c8b9dd17234f6a56d2c602db82f51578d11
wh6879 <wh6879@aliyun.com>
2023-04-02 18:21:12 +0800

    auth: wh commit: ProducerPart

---
+++ src/main/java/org/example/partition/ProducerCallbackPartRecord.java
@@ -0,0 +1,32 @@
+package org.example.partition;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerCallbackPartRecord {
+    public static void main(String[] args) {
+
+        // 0
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.96.131:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        for (int i = 0; i < 3; i++) {
+            ProducerRecord record = new ProducerRecord<>("first", 2, "", i + "--test");
+            producer.send(record, (metadata, exception) -> {
+                if (exception == null) {
+                    System.out.println("topic :" + metadata.topic() + " partition: " + metadata.partition());
+                }
+            });
+        }
+
+        producer.close();
+    }
+}
