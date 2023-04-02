1715a5597d56fb7cc5a63864f9656e54d61da6c7
wh6879 <wh6879@aliyun.com>
2023-04-02 17:13:57 +0800

    auth: wh commit: kafka-producer

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // 0
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.96.131:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        for (int i = 0; i < 3; i++) {
+            ProducerRecord record = new ProducerRecord<>("first", i + "--test");
+            producer.send(record);
+        }
+
+        producer.close();
     }
 }
