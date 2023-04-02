6e0266cffb541cee40856bdbdd42ccfc75144e36
wh6879 <wh6879@aliyun.com>
2023-04-02 21:00:06 +0800

    auth: wh commit: ProducerTransaction

---
+++ src/main/java/org/example/transaction/ProducerTransaction.java
@@ -0,0 +1,37 @@
+package org.example.transaction;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.errors.ProducerFencedException;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerTransaction {
+    public static void main(String[] args) {
+
+        // 0
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.96.131:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        producer.initTransactions();
+        producer.beginTransaction();
+
+        try {
+            for (int i = 0; i < 3; i++) {
+                ProducerRecord record = new ProducerRecord<>("first", i + "--test");
+                producer.send(record);
+            }
+            producer.commitTransaction();
+        } catch (Exception e) {
+            producer.abortTransaction();
+        } finally {
+            producer.close();
+        }
+    }
+}
