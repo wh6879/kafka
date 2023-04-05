1a1e73b30b0e08e8d35d6b8b4646addf1fb3662a
wh6879 <wh6879@aliyun.com>
2023-04-05 18:10:41 +0800

    auth: wh commit: consumer partation strategy

---
+++ src/main/java/org/example/consumer/partitonstrategy/range/CustomConsumer3.java
@@ -0,0 +1,45 @@
+package org.example.consumer.partitonstrategy.range;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class CustomConsumer3 {
+
+    public static void main(String[] args) {
+        // 0 配置
+        Properties properties = new Properties();
+
+        // 连接
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.96.137:9092");
+
+        // 反序列化
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        // groupid
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "GroupId2");
+
+        // 1 创建消费者
+        KafkaConsumer consumer = new KafkaConsumer(properties);
+
+        // 2 订阅
+        ArrayList<String> topics = new ArrayList<>();
+        topics.add("first");
+        consumer.subscribe(topics);
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
+            for (ConsumerRecord record : records) {
+                System.out.println(record);
+            }
+        }
+
+    }
+}
